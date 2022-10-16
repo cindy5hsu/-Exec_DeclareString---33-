dda12ca6b99563fbdab39f506336c4340accd09b
Cindy <Cindy@LAPTOP-I3P14G10>
2022-10-16 22:03:59 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string value1 = "c:\\temp\\a.jpg";
+            Console.WriteLine(value1);
+
+            string value2 = "我是一個 \"好學生\"";
+            Console.WriteLine(value2);
+
+            string value3 = "我是第一行\n我是第二行";
+            Console.WriteLine(value3);
+        }
+    }
+}
